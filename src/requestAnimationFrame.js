db178ef52be9564f1e281777efeb8726d5104fa7
Dmitry Shur <dimashur@gmail.com>
2018-05-11 20:59:41 +0300

    added raf function

---
+++ src/requestAnimationFrame.js
@@ -0,0 +1,23 @@
+(function _raf() {
+  const w = window;
+  ['ms', 'moz', 'webkit', 'o'].forEach(vendor => {
+    if (!w.requestAnimationFrame) {
+      w.requestAnimationFrame = w[`${vendor}RequestAnimationFrame`];
+      w.cancelAnimationFrame =
+        w[`${vendor}CancelAnimationFrame`] || w[`${vendor}CancelRequestAnimationFrame`];
+    }
+  });
+
+  if (!w.requestAnimationFrame) {
+    w.requestAnimationFrame = callback => {
+      let currentTime;
+      let targetTime = 0;
+      targetTime = Math.max(targetTime + 16, (currentTime = +new Date()));
+      return w.setTimeout(() => callback(+new Date()), targetTime - currentTime);
+    };
+  }
+
+  if (!w.cancelAnimationFrame) {
+    w.cancelAnimationFrame = id => clearTimeout(id);
+  }
+})();
